1218ec2060b14c4a80ca426acc1a8a6919fe82d8
David Scott Perez <david.scott.perez@live.com>
2018-07-18 07:21:11 -0700

    added form + view for artist displays

--- routes/index.js
+++ routes/index.js
@@ -39,8 +39,42 @@ router.get('/artist/:id', function(req, res, next) {
       res.send('not a valid id');
     }
   }); 
+  
 });
 
+const artistList = `SELECT * FROM artists`;
+
+router.get('/artist', function(req, res, next) {
+  db.all(artistList, function(err, row) {
+    res.render('artist', {
+      artists: row
+    });
+  });
+});
+
+const newArtist = db.prepare(`INSERT INTO artists(Name) VALUES(?)`);
+
+router.post('/artist', function(req, res, next) {
+  console.log(req.body.name);
+  const addArtist = req.body.name;
+  
+  const selectArtist = `SELECT * FROM artists WHERE artists.name = '${addArtist}'`;
+
+  db.all(selectArtist, function(err, row) {
+    if (row.length > 0) {
+      res.send('Sorry, that artist already exists');
+      //this is needed because the row is an array so if there is anything in the array, that means there is an artist with the same name as req.body.params. If the row is zero, that means there is no artist in the database with that name.
+    } else {
+      newArtist.all(addArtist, function(err, row) {
+        if (err){
+          res.render('/error', { message: 'oops, something went wrong!'})
+        } else {
+          res.redirect('/artist');
+        }
+      });
+    }
+  });
+});
 
 
 module.exports = router;
