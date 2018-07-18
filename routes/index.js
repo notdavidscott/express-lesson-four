cb447712a5cdf8b21e689e1e84a4d902533ea04a
David Scott Perez <david.scott.perez@live.com>
2018-07-17 23:52:11 -0700

    using database to display artist names
    based off of id## in url

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
-var express = require('express');
+const express = require('express');
 var router = express.Router();
+const sqlite = require('sqlite3').verbose();
+
+  const db = new sqlite.Database('./chinook.sqlite', err => {
+    if (err) {
+      return console.log(err.message);
+    }
+    console.log("<< | You are connected to the database | >>")
+  });
+
+  const query = `SELECT * from artists LIMIT 10`;
+
+  db.all(query, (err, row) => {
+    if (err) throw err;
+    console.log(row);
+  });
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/artist/:id', function(req, res, next) {
+  let artist = parseInt(req.params.id);
+  console.log(artist);
+
+  let idQuery = `SELECT * FROM artists WHERE ArtistId=${artist}`;
+  console.log(idQuery);
+
+  db.all(idQuery, (err, row) => {
+    console.log(row);
+    if (row.length > 0) {
+      res.render('index', {
+        artist: row[0]  
+      });
+    } else {
+      res.send('not a valid id');
+    }
+  }); 
+});
+
+
 
 module.exports = router;
